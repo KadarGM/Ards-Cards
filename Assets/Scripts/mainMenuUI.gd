eb3996173762bbb4b3b3314654fbaee0701a6d66
KadarGM <jackob.zummer@gmail.com>
2024-03-31 18:26:51 +0200

    UI

--- Assets/Scripts/mainMenuUI.gd
+++ Assets/Scripts/mainMenuUI.gd
@@ -4,69 +4,64 @@ extends CanvasLayer
 @onready var decks_menu = $ManageDecks
 @onready var setting_menu = $SettingMenu
 @onready var logo = $Logo
+@onready var logo_rect = $Logo/TextureRect
 
 var tween: Tween
 
-func _ready():
-	menu.visible = false
-	decks_menu.visible = false
-	setting_menu.visible = false
-	logo.visible = false
+func animation(type,dir1,dir2,cond):
+	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(type, "position", Vector2(dir1,dir2), 0.5)
+	if cond == false:
+		tween.tween_property(type, "visible", false, 0.1)
+	else:
+		type.visible = true
+	await get_tree().create_timer(0.4).timeout
+
+func start():
 	menu.position = Vector2(-400,0)
 	decks_menu.position = Vector2(-400,0)
 	setting_menu.position = Vector2(-400,0)
-	logo.position = Vector2(logo.size.x,0)
-	animation()
+	logo_rect.position = Vector2(logo.size.x,(logo.size.y - logo_rect.size.y)/2)
 
-func animation():
-	menu.visible = true
+	menu.visible = false
+	decks_menu.visible = false
+	setting_menu.visible = false
 	logo.visible = true
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(logo, "position", Vector2(0,0), 0.5)
-	tween.tween_property(menu, "position", Vector2(0,0), 0.5)
+
+	animation(logo_rect,logo.size.x - 100 - logo_rect.size.x,(logo.size.y - logo_rect.size.y)/2,true)
+	animation(menu,0,0,true)
+
+func _ready():
+	start()
 
 func _on_play_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(logo, "position", Vector2(logo.size.x,0), 0.7)
-	tween.tween_property(menu, "visible", false, 0.2)
-	tween.tween_property(logo, "visible", false, 0.2)
+	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(menu,-400,0,false)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Assets/Scenes/game.tscn")
 
 func _on_manage_deck_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(decks_menu, "position", Vector2(0,0), 0.5)
-	tween.tween_property(menu, "visible", false, 0.2)
-	decks_menu.visible = true
+	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(decks_menu,0,0,true)
+	animation(menu,-400,0,false)
 
 func _on_setting_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(setting_menu, "position", Vector2(0,0), 0.5)
-	tween.tween_property(menu, "visible", false, 0.2)
-	setting_menu.visible = true
+	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(setting_menu,0,0,true)
+	animation(menu,-400,0,false)
 
 func _on_back_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(decks_menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(menu, "position", Vector2(0,0), 0.5)
-	tween.tween_property(decks_menu, "visible", false, 0.2)
-	menu.visible = true
+	animation(logo_rect,logo.size.x - 100 - logo_rect.size.x,(logo.size.y - logo_rect.size.y)/2,true)
+	animation(menu,0,0,true)
+	animation(decks_menu,-400,0,false)
 
 func _on_back_setting_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(setting_menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(menu, "position", Vector2(0,0), 0.5)
-	tween.tween_property(setting_menu, "visible", false, 0.2)
-	menu.visible = true
-	
+	animation(logo_rect,logo.size.x - 100 - logo_rect.size.x,(logo.size.y - logo_rect.size.y)/2,true)
+	animation(menu,0,0,true)
+	animation(setting_menu,-400,0,false)
+
 func _on_exit_button_pressed():
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(menu, "position", Vector2(-400,0), 0.5)
-	tween.tween_property(logo, "position", Vector2(logo.size.x,0), 0.7)
-	tween.tween_property(menu, "visible", false, 0.2)
-	tween.tween_property(logo, "visible", false, 0.2)
+	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(menu,-400,0,false)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
