433d6df79fb3cae4b0520ff0bb5e37f521171bc7
KadarGM <jackob.zummer@gmail.com>
2024-03-31 22:39:27 +0200

    UI changes

--- Assets/Scripts/mainMenuUI.gd
+++ Assets/Scripts/mainMenuUI.gd
@@ -10,18 +10,18 @@ var tween: Tween
 
 func animation(type,dir1,dir2,cond):
 	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(type, "position", Vector2(dir1,dir2), 0.5)
+	tween.tween_property(type, "position", Vector2(dir1,dir2), 0.7)
 	if cond == false:
 		tween.tween_property(type, "visible", false, 0.1)
 	else:
 		type.visible = true
-	await get_tree().create_timer(0.4).timeout
 
 func start():
 	menu.position = Vector2(-400,0)
 	decks_menu.position = Vector2(-400,0)
 	setting_menu.position = Vector2(-400,0)
-	logo_rect.position = Vector2(logo.size.x,(logo.size.y - logo_rect.size.y)/2)
+	logo_rect.scale *= logo.size.x/1000
+	logo_rect.position = Vector2(logo.size.x + 1000,(logo.size.y - logo_rect.size.y)/2)
 
 	menu.visible = false
 	decks_menu.visible = false
@@ -35,18 +35,18 @@ func _ready():
 	start()
 
 func _on_play_button_pressed():
-	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(logo_rect,logo.size.x + 1000,(logo.size.y - logo_rect.size.y)/2,false)
 	animation(menu,-400,0,false)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Assets/Scenes/game.tscn")
 
 func _on_manage_deck_button_pressed():
-	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(logo_rect,logo.size.x + 1000,(logo.size.y - logo_rect.size.y)/2,false)
 	animation(decks_menu,0,0,true)
 	animation(menu,-400,0,false)
 
 func _on_setting_button_pressed():
-	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(logo_rect,logo.size.x + 1000,(logo.size.y - logo_rect.size.y)/2,false)
 	animation(setting_menu,0,0,true)
 	animation(menu,-400,0,false)
 
@@ -61,7 +61,7 @@ func _on_back_setting_button_pressed():
 	animation(setting_menu,-400,0,false)
 
 func _on_exit_button_pressed():
-	animation(logo_rect,logo.size.x,(logo.size.y - logo_rect.size.y)/2,false)
+	animation(logo_rect,logo.size.x + 1000,(logo.size.y - logo_rect.size.y)/2,false)
 	animation(menu,-400,0,false)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
