38d9679774bd5d49f174692fd34cc7a7dbaba15d
KadarGM <jackob.zummer@gmail.com>
2024-04-02 21:41:19 +0200

    meh

--- Assets/Scripts/mainMenuUI.gd
+++ Assets/Scripts/mainMenuUI.gd
@@ -1,62 +1,52 @@
 extends CanvasLayer
 
 @onready var menu = $MainMenu
-@onready var menu_hbox = $MainMenu/MarginContainer/HBoxContainer
 @onready var decks_menu = $ManageDecks
-@onready var decks_hbox = $ManageDecks/MarginContainer/HBoxContainer
-@onready var setting_menu = $SettingMenu
-@onready var setting_menu_hbox = $SettingMenu/MarginContainer/HBoxContainer
-@onready var logo = $Logo
-@onready var setting_setup_hbox = $SettingSetup/MarginContainer/HBoxContainer
-
-var tween: Tween
+@onready var setting = $SettingSetup
+@onready var ui_scaler_text = $SettingSetup/HBoxContainer/ChoiceVBox/HBoxContainer/UIScalerText
+@onready var ui_scaler = $SettingSetup/HBoxContainer/ChoiceVBox/HBoxContainer/UIScaler
 
 func _ready():
 	start()
-
-func animation(type,dir1,dir2,cond,anim,time):
-	tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	tween.tween_property(type, anim, Vector2(dir1,dir2), time)
-	if cond == false:
-		tween.tween_property(type, "visible", false, 0.1)
-	else:
-		type.visible = true
+	set_scale_on_start()
 
 func start():	
-	menu.position = Vector2(-400,0)
-	decks_menu.position = Vector2(-400,0)
-	setting_menu.position = Vector2(-400,0)
-	
-	menu.visible = false
+	menu.visible = true
 	decks_menu.visible = false
-	setting_menu.visible = false
-	logo.visible = true
-
-	animation(menu,0,0,true,"position",0.7)
-	animation(logo,0,0,false,"position",0.7)
+	setting.visible = false
+	setting.pivot_offset = setting.size/2
+	decks_menu.pivot_offset = decks_menu.size/2
+	menu.pivot_offset = menu.size/2
 
 func _on_play_button_pressed():
-	animation(menu,-400,0,false,"position",0.7)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_file("res://Assets/Scenes/game.tscn")
 
 func _on_manage_deck_button_pressed():
-	animation(decks_menu,0,0,true,"position",0.7)
-	animation(menu,-400,0,false,"position",0.7)
+	menu.visible = false
+	decks_menu.visible = true
 
 func _on_setting_button_pressed():
-	animation(setting_menu,0,0,true,"position",0.7)
-	animation(menu,-400,0,false,"position",0.7)
+	menu.visible = false
+	setting.visible = true
 
 func _on_back_button_pressed():
-	animation(menu,0,0,true,"position",0.7)
-	animation(decks_menu,-400,0,false,"position",0.7)
-
-func _on_back_setting_button_pressed():
-	animation(menu,0,0,true,"position",0.7)
-	animation(setting_menu,-400,0,false,"position",0.7)
+	decks_menu.visible = false
+	menu.visible = true
 
 func _on_exit_button_pressed():
-	animation(menu,-400,0,false,"position",0.7)
 	await get_tree().create_timer(1.0).timeout
 	get_tree().quit()
+
+func _on_back_setting_button_pressed():
+	setting.visible = false
+	menu.visible = true
+
+func set_scale_on_start():
+	ui_scaler_text.set_text("UI: " + str(ui_scaler.value) + "%")
+
+func _on_ui_scaler_value_changed(value):
+	ui_scaler_text.set_text("UI: " + str(value) + "%")
+	setting.scale = Vector2(value/100, value/100)
+	menu.scale = Vector2(value/100, value/100)
+	decks_menu.scale = Vector2(value/100, value/100)
--- Assets/Scripts/settingSetup.gd
+++ Assets/Scripts/settingSetup.gd
@@ -1,34 +1,58 @@
 extends Control
 
-@onready var resolution_menu = $MarginContainer/HBoxContainer/ChoiceVBox/ResolutionMenu
-@onready var screen_menu = $MarginContainer/HBoxContainer/ChoiceVBox/ScreenMenu
-@onready var scale_slider = $MarginContainer/HBoxContainer/ChoiceVBox/ScaleBox/ScaleSlider
-@onready var scale_label = $MarginContainer/HBoxContainer/ChoiceVBox/ScaleBox/ScaleLabel
-@onready var fullscreen_box = $MarginContainer/HBoxContainer/ChoiceVBox/FullscreenBox
-@onready var fsr_options = $MarginContainer/HBoxContainer/ChoiceVBox/FSROptions
+@onready var resolution_menu = $HBoxContainer/ChoiceVBox/ResolutionMenu
+@onready var screen_menu = $HBoxContainer/ChoiceVBox/ScreenMenu
+@onready var fullscreen_box = $HBoxContainer/ChoiceVBox/FullscreenBox
+@onready var borderless_box = $HBoxContainer/ChoiceVBox/BorderlessBox
+@onready var ui_scaler = $HBoxContainer/ChoiceVBox/HBoxContainer/UIScaler
+@onready var ui_scaler_text = $HBoxContainer/ChoiceVBox/HBoxContainer/UIScalerText
 
 var Resolutions : Dictionary = {
-	"3840x2160":Vector2i(3840,2160),
-	"2560x1440":Vector2i(2560,1080),
-	"1920x1080":Vector2i(1920,1080),
-	"1280x720":Vector2i(1280,720),
+	"3840 x 2160":Vector2i(3840,2160),
+	"2560 x 1440":Vector2i(2560,1080),
+	"1920 x 1080":Vector2i(1920,1080),
+	"1280 x 720":Vector2i(1280,720),
 	}
 
 func _ready():
 	add_resolutions()
 	check_variables()
+	get_screens()
+	check_texts()
 
 func check_variables():
 	var _window = get_window()
 	var mode = _window.get_mode()
 	
-	if mode == Window.MODE_FULLSCREEN:
+	if mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
 		resolution_menu.set_disabled(true)
 		fullscreen_box.set_pressed_no_signal(true)
+
+func check_texts():
+	remove_resolutions()
+	add_resolutions()
+	get_tree().create_timer(.05).timeout.connect(set_resolution_text)
+	get_tree().create_timer(.05).timeout.connect(change_borderless_text)
+	get_tree().create_timer(.05).timeout.connect(set_monitor_text)
 
 func set_resolution_text():
 	var resolution_text = str(get_window().get_size().x) + "x" + str(get_window().get_size().y)
 	resolution_menu.set_text(resolution_text)
+
+func set_monitor_text():
+	var active_screen =  DisplayServer.window_get_current_screen()
+	var monitor_text = "Screen: "+str(active_screen)
+	screen_menu.set_text(monitor_text)
+	screen_menu.select(active_screen)
+
+func change_borderless_text():
+	var _window = get_window()
+	var mode = _window.get_mode()
+
+	if mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+		borderless_box.text = "Exclusive"
+	else:
+		borderless_box.text = "Borderless"
 
 func add_resolutions():
 	var current_resolution = get_window().get_size()
@@ -39,10 +63,19 @@ func add_resolutions():
 			resolution_menu.select(ID)
 		ID += 1
 
+func remove_resolutions():
+	var current_resolution = get_window().get_size()
+	var ID = Resolutions.size()
+	print(Resolutions.size())
+	for r in range(ID+1):
+		resolution_menu.remove_item(ID)
+		ID -= 1
+
 func _on_resolution_menu_item_selected(index):
 	var ID = resolution_menu.get_item_text(index)
 	get_window().set_size(Resolutions[ID])
 	centre_window()
+	get_tree().create_timer(.05).timeout.connect(set_monitor_text)
 
 func centre_window():
 	var center_screen = DisplayServer.screen_get_position() + DisplayServer.screen_get_size()/2
@@ -52,8 +85,56 @@ func centre_window():
 func _on_fullscreen_box_toggled(toggled_on):
 	resolution_menu.set_disabled(toggled_on)
 	if toggled_on:
-		get_window().set_mode(Window.MODE_FULLSCREEN)
+		get_window().set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
+		borderless_box.button_pressed = true
+		borderless_box.text = "Exclusive"
 	else:
 		get_window().set_mode(Window.MODE_WINDOWED)
+		borderless_box.button_pressed = false
+		borderless_box.text = "Borderless"
 		centre_window()
-	get_tree().create_timer(.05).timeout.connect(set_resolution_text)
+	check_texts()
+
+func get_screens():
+	var screens = DisplayServer.get_screen_count()
+	if screens > 1:
+		screen_menu.visible = true
+		for s in screens:
+			screen_menu.add_item("Screen: "+str(s))
+	else:
+		screen_menu.visible = false
+
+func _on_screen_menu_item_selected(index):
+	var _window = get_window()
+	var mode = _window.get_mode()
+
+	_window.set_mode(Window.MODE_WINDOWED)
+	_window.set_current_screen(index)
+
+	if mode == Window.MODE_EXCLUSIVE_FULLSCREEN:
+		_window.set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
+	check_texts()
+
+func _on_borderless_box_toggled(toggled_on):
+	var _window = get_window()
+	var mode = _window.get_mode()
+
+	if toggled_on:
+		if mode == Window.MODE_EXCLUSIVE_FULLSCREEN or mode == Window.MODE_FULLSCREEN:
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			_window.set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
+			_window.set_mode(Window.MODE_WINDOWED)
+	else:
+		if mode == Window.MODE_EXCLUSIVE_FULLSCREEN or mode == Window.MODE_FULLSCREEN:
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			_window.set_mode(Window.MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, false)
+			_window.set_mode(Window.MODE_WINDOWED)
+			borderless_box.button_pressed = false
+	check_texts()
+
+
+
