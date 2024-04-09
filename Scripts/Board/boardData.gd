91074f58fd9607876016fcd5555fe05181ef6a58
KadarGM <130982681+KadarGM@users.noreply.github.com>
2024-04-09 18:41:01 +0200

    lot of work

---
+++ Scripts/Board/boardData.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+#@onready var attack_slot_1 = $AttackSlot_1
+#
+#
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#GameDataManager.get_slot(attack_slot_1)
