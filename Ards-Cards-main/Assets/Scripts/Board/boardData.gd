3e7d22aabdcd1dcaefb909b34b170556e2405906
KadarGM <jackob.zummer@gmail.com>
2024-04-10 08:56:46 +0200

    lot of changes uff

--- Ards-Cards-main/Assets/Scripts/Board/boardData.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-#@onready var attack_slot_1 = $AttackSlot_1
-#
-#
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-	#GameDataManager.get_slot(attack_slot_1)
--- Scripts/Global/cardDataList.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-var card : Dictionary = {
-	0 : ["Mlok", 2,5, 3, 4],
-	1 : ["Pepa",4,1, 2, 3],
-	2 : ["Ivan",5,3, 1, 0],
-	3 : ["Josef", 1,4, 4, 5]
-}
